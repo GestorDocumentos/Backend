83ea40026dac0f63d9b5ff33e048f5e840e86f5f
ian soto <ian.soto.4798@gmail.com>
2026-04-12 20:14:39 -0500

    Cambios minimos DTO

--- src/main/java/com/eam/demo/business/dto/DocumentDTO.java
+++ src/main/java/com/eam/demo/business/dto/DocumentDTO.java
@@ -17,13 +17,13 @@ public class DocumentDTO {
     @Schema(description = "ID unico del documento", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
-    @Schema(description = "Tipo del documento", example = "ACTA", requiredMode = Schema.RequiredMode.REQUIRED, maxLength = 100)
+    @Schema(description = "Tipo del documento", example = "PDF", requiredMode = Schema.RequiredMode.REQUIRED, maxLength = 100)
     private String tipo;
 
     @Schema(description = "Fecha del documento", example = "2026-04-12", requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDate fecha;
 
-    @Schema(description = "Estado del documento", example = "PENDING", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "Estado del documento", example = "Pendiente", requiredMode = Schema.RequiredMode.REQUIRED)
     private DocumentStatusEntity estado;
 
     @Schema(description = "ID del usuario asociado al documento", example = "10", requiredMode = Schema.RequiredMode.REQUIRED)
